e894561f717b706b876416a20a8578bca9f49be5
Schiggy <hillbrecht.s@gmail.com>
2026-09-14 18:36:02 +0200

    added volumn to sound Manager

--- audio_manager.gd
+++ audio_manager.gd
@@ -18,7 +18,7 @@ enum Sound {}
 const SFX_BUS := &"SFX"
 const SOUNDS := {}
 
-func play_sfx(sound: Sound, pitch_variation := 0.0) -> void:
+func play(sound: Sound, pitch_variation := 0.0, volume := 0.0) -> void:
 	var stream: AudioStream = SOUNDS.get(sound)
 	if stream == null:
 		return
@@ -26,11 +26,12 @@ func play_sfx(sound: Sound, pitch_variation := 0.0) -> void:
 	player.stream = stream
 	player.bus = SFX_BUS
 	player.pitch_scale = randf_range(1.0 - pitch_variation, 1.0 + pitch_variation)
+	player.volume_db = volume
 	add_child(player)
 	player.finished.connect(player.queue_free)
 	player.play()
 
-func play_sfx_2d(sound: Sound, global_position: Vector2, pitch_variation := 0.0) -> void:
+func play_at_pos(sound: Sound, global_position: Vector2, pitch_variation := 0.0) -> void:
 	var stream: AudioStream = SOUNDS.get(sound)
 	if stream == null:
 		return
